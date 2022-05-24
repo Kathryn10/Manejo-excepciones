9b44814e989f31acf54c5832f65dd61e8e3ff4ee
Kathryn10 <105743488+Kathryn10@users.noreply.github.com>
2022-05-24 11:22:25 -0700

    Manejo-excepciones
    Segundo programa de excepciones

---
+++ Manejo-excepciones/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manejo_excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            int Resultado;
+
+            try
+            {
+                Console.Write("Ingrese un núnero entero: ");
+                num = int.Parse(Console.ReadLine());
+
+                Resultado = num * num;
+
+                Console.WriteLine("\nEl cuadrado de " + num + " es: " + Resultado);
+                Console.ReadLine();
+            }
+            catch(OverflowException)
+            {
+                Console.WriteLine("\nNúmero demasiado grande");
+                Console.ReadLine();
+            }
+            catch (FormatException x)
+            {
+                Console.WriteLine(x.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
